6df565e58195c06d0c28d2d6ed92628e980ecd8f
Joao Marcos <joaomarcos11.tech@gmail.com>
2024-09-25 22:29:00 -0300

    Update api-gw.tf

--- api-gw.tf
+++ api-gw.tf
@@ -53,7 +53,7 @@ resource "aws_api_gateway_deployment" "deployment" {
     # aws_api_gateway_integration.options_integration, # Add this line
   ]
 
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  rest_api_id = aws_api_gateway_rest_api.fiap44.id
   stage_name = "prd"
 }
 
